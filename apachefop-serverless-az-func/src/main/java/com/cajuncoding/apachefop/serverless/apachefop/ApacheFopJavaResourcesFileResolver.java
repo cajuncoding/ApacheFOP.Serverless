563aeea13afef36b4a654ec99ea82fedd2d5835b
Brandon Bernard <brandon.bernard@exlrt.com>
2021-10-26 02:26:40 -0500

    Initial work to resolve VS Code issues for compiling and running; updated to latest Microsoft Azure Functions Maven Plugin, and removed GUID naming that was previously added but breaks Debug launching in VS Code.
    Initial fixes for bugs when loading local Fonts vs Font Resources via Azure Function running with Windows OS.

--- apachefop-serverless-az-func/src/main/java/com/cajuncoding/apachefop/serverless/apachefop/ApacheFopJavaResourcesFileResolver.java
+++ apachefop-serverless-az-func/src/main/java/com/cajuncoding/apachefop/serverless/apachefop/ApacheFopJavaResourcesFileResolver.java
@@ -27,13 +27,13 @@ public class ApacheFopJavaResourcesFileResolver implements ResourceResolver {
     public Resource getResource(URI uri) throws IOException {
         InputStream resourceStream = null;
 
-        //We MUST ONLY attempt to handle FILE requests... any Http/Https requests need to use original/default behaviour!
+        //We MUST ONLY attempt to handle FILE requests... any Http/Https requests need to use original/default behavior!
         if(uri.getScheme().equalsIgnoreCase(FILE_SCHEME)) {
-            resourceStream = findJavaResourceWithCaching(uri);
+            resourceStream = tryFindJavaResourceWithCaching(uri);
         }
 
         //If the resource was located then we return it...
-        //Otherwise we fallback to default Apache FOP behaviour using the original default Fop Resource Resolver...
+        //Otherwise we fallback to default Apache FOP behavior using the original default Fop Resource Resolver...
         return resourceStream != null
             ? new Resource(resourceStream)
             : defaultFopResolver.getResource(uri);
@@ -52,23 +52,33 @@ public class ApacheFopJavaResourcesFileResolver implements ResourceResolver {
 
     //Find the Resource and utilize the internal cache for performance since Embedded Resources can't change without
     //  deployments to update, we can keep these elements (e.g. Fonts) loaded in Memory for performance!
-    protected InputStream findJavaResourceWithCaching(URI uri) {
+    protected InputStream tryFindJavaResourceWithCaching(URI uri) {
+        var requestPath = Paths.get(uri);
 
         //Use cached results from our Concurrent HashMap cached data if possible!
         var resourceBytes = javaResourceFileCache.computeIfAbsent(uri, key -> {
-
             //Map the requested Uri to the base application path to determine it's relative path as a Resource!
-            var requestPath = Paths.get(uri);
-            var mappedPath = ResourceUtils.MapServerPath(requestPath);
-
-            //Now with the relative path for our resource we can attempt to retrieve it...
-            var resultStream = ResourceUtils.loadResourceAsStream(mappedPath.toString());
             try {
+                var mappedPath = ResourceUtils.MapServerPath(requestPath);
+                
+                //If the path can't be safely mapped we return null to be cached so future lookups are not attempted.
+                //NOTE: So that we don't black hole this we attempt to log to the system out...
+                if(mappedPath == null) {
+                    System.out.print("Java Resource Path could not be resolved for: " + uri.toString());
+                    return null;
+                }
 
-                return resultStream != null ? IOUtils.toByteArray(resultStream) : null;
-
-            } catch (IOException e) {
+                //Now with the relative path for our resource we can attempt to retrieve it...
+                var resultStream = ResourceUtils.loadResourceAsStream(mappedPath.toString());
+                
+                return resultStream != null 
+                    ? IOUtils.toByteArray(resultStream) 
+                    : null;                
+            } 
+            catch (Exception e) {
                 //e.printStackTrace();
+                //NOTE: So that we don't black hole this we attempt to log to the system out...
+                System.out.print("EXCEPTION occurred trying to resolve Java Resource for [" + uri.toString() + "]: " + e.getMessage());
                 return null;
             }
         });
